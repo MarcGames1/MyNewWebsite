f2cb9d33448b08ea4dbf8aa314e20705b7d5c836
MarcGames1 <George.marcu20@gmail.com>
2021-07-22 06:13:28 -0700

    sticky menu bug

--- pages/creare-website.tsx
+++ pages/creare-website.tsx
@@ -11,81 +11,81 @@ import FormularText from "../components/Formular+Text/Formular+Text"
 
 function Page() {
 
-const [level, setLevel] =useState(1)
-
-let cuprins =[
   
-  new CuprinsElement('1', 'Reprezentarea vizuala a websiteului', '#1'),
-  new CuprinsElement('1.1','MoodBord','#1.1'),
-  new CuprinsElement('1.2','Wireframe','#1.2'),
-  new CuprinsElement('1.3','mockup','#1.3'),
-  new CuprinsElement('2','Pregatirea resurselor pentru web','#2'),
-  new CuprinsElement('3','Dezvoltarea efectiva a website-ului','#3'),
-  new CuprinsElement('3.1','Testare','#3.1'),
-  new CuprinsElement('3.2','Debug','#3.2'),
-  new CuprinsElement('3.3','Optimizare','#3.3'),
-  new CuprinsElement('4','De ce sa lucrezi cu mine', '#4' )
-]
-  
-const texte = [
-'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit nam, magni natus repellendus quod dolore expedita provident at quas fuga, officiis quasi qui ratione atque rerum! Mollitia hic maiores ducimus.',
-'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit nam, magni natus repellendus quod dolore expedita provident at quas fuga, officiis quasi qui ratione atque rerum! Mollitia hic maiores ducimus.',
-'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit nam, magni natus repellendus quod dolore expedita provident at quas fuga, officiis quasi qui ratione atque rerum! Mollitia hic maiores ducimus.',
 
-] 
-    return(<>
-      <MainNav />
-      <Container fluid>
+  let cuprins = [
+
+    new CuprinsElement('1', 'Reprezentarea vizuala a websiteului', '#1'),
+    new CuprinsElement('1.1', 'MoodBord', '#1.1'),
+    new CuprinsElement('1.2', 'Wireframe', '#1.2'),
+    new CuprinsElement('1.3', 'mockup', '#1.3'),
+    new CuprinsElement('2', 'Pregatirea resurselor pentru web', '#2'),
+    new CuprinsElement('3', 'Dezvoltarea efectiva a website-ului', '#3'),
+    new CuprinsElement('3.1', 'Testare', '#3.1'),
+    new CuprinsElement('3.2', 'Debug', '#3.2'),
+    new CuprinsElement('3.3', 'Optimizare', '#3.3'),
+    new CuprinsElement('4', 'De ce sa lucrezi cu mine', '#4')
+  ]
+
+  const beneficii = [
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit nam, magni natus repellendus quod dolore expedita provident at quas fuga, officiis quasi qui ratione atque rerum! Mollitia hic maiores ducimus.',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit nam, magni natus repellendus quod dolore expedita provident at quas fuga, officiis quasi qui ratione atque rerum! Mollitia hic maiores ducimus.',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit nam, magni natus repellendus quod dolore expedita provident at quas fuga, officiis quasi qui ratione atque rerum! Mollitia hic maiores ducimus.',
+
+  ]
+  return (<>
+    <MainNav />
+    <Container fluid>
       <h1 className='text-center'>Creare Site Web</h1>
-<Row>
-  <Col className='sticky-lg-top' lg={3}><Cuprins active={false} list={cuprins} /></Col>
-  <Col> <article>
-       <h2>Procesul de Creare a unui site web</h2>
+      <Row>
+        <Col lg={3}><Cuprins active={false} list={cuprins} /></Col>
+        <Col> <article>
+          <h2>Procesul de Creare a unui site web</h2>
 
 
-    
-      <HeaderInFunctieDeCuprins {...cuprins[0]} />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem rerum fugit, nostrum corrupti, esse perferendis eligendi optio quas odio fugiat autem ut repellat accusantium quaerat quis. Quo dolorum cumque in?</p>
-      <HeaderInFunctieDeCuprins {...cuprins[1]} />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur excepturi iusto itaque harum facere. Sequi nostrum deleniti est. Tempore ipsum quos odit cumque? Corporis veritatis, laudantium eos fugit nisi iste?</p>
-      <HeaderInFunctieDeCuprins {...cuprins[2]} />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur excepturi iusto itaque harum facere. Sequi nostrum deleniti est. Tempore ipsum quos odit cumque? Corporis veritatis, laudantium eos fugit nisi iste?</p>
-      <HeaderInFunctieDeCuprins {...cuprins[3]} />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur excepturi iusto itaque harum facere. Sequi nostrum deleniti est. Tempore ipsum quos odit cumque? Corporis veritatis, laudantium eos fugit nisi iste?</p>
-      <HeaderInFunctieDeCuprins {...cuprins[4]} />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur excepturi iusto itaque harum facere. Sequi nostrum deleniti est. Tempore ipsum quos odit cumque? Corporis veritatis, laudantium eos fugit nisi iste?</p>
-      <HeaderInFunctieDeCuprins {...cuprins[5]} />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur excepturi iusto itaque harum facere. Sequi nostrum deleniti est. Tempore ipsum quos odit cumque? Corporis veritatis, laudantium eos fugit nisi iste?</p>
-      <HeaderInFunctieDeCuprins {...cuprins[6]} />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur excepturi iusto itaque harum facere. Sequi nostrum deleniti est. Tempore ipsum quos odit cumque? Corporis veritatis, laudantium eos fugit nisi iste?</p>
-      <HeaderInFunctieDeCuprins {...cuprins[7]} />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur excepturi iusto itaque harum facere. Sequi nostrum deleniti est. Tempore ipsum quos odit cumque? Corporis veritatis, laudantium eos fugit nisi iste?</p>
-      <HeaderInFunctieDeCuprins {...cuprins[8]} />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur excepturi iusto itaque harum facere. Sequi nostrum deleniti est. Tempore ipsum quos odit cumque? Corporis veritatis, laudantium eos fugit nisi iste?</p>
-      <HeaderInFunctieDeCuprins {...cuprins[9]} />
 
-      <FormularText text={texte}/>
-    
-     </article></Col>
-  {/* <Col></Col> */}
-</Row>
-     
+          <HeaderInFunctieDeCuprins {...cuprins[0]} />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem rerum fugit, nostrum corrupti, esse perferendis eligendi optio quas odio fugiat autem ut repellat accusantium quaerat quis. Quo dolorum cumque in?</p>
+          <HeaderInFunctieDeCuprins {...cuprins[1]} />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur excepturi iusto itaque harum facere. Sequi nostrum deleniti est. Tempore ipsum quos odit cumque? Corporis veritatis, laudantium eos fugit nisi iste?</p>
+          <HeaderInFunctieDeCuprins {...cuprins[2]} />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur excepturi iusto itaque harum facere. Sequi nostrum deleniti est. Tempore ipsum quos odit cumque? Corporis veritatis, laudantium eos fugit nisi iste?</p>
+          <HeaderInFunctieDeCuprins {...cuprins[3]} />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur excepturi iusto itaque harum facere. Sequi nostrum deleniti est. Tempore ipsum quos odit cumque? Corporis veritatis, laudantium eos fugit nisi iste?</p>
+          <HeaderInFunctieDeCuprins {...cuprins[4]} />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur excepturi iusto itaque harum facere. Sequi nostrum deleniti est. Tempore ipsum quos odit cumque? Corporis veritatis, laudantium eos fugit nisi iste?</p>
+          <HeaderInFunctieDeCuprins {...cuprins[5]} />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur excepturi iusto itaque harum facere. Sequi nostrum deleniti est. Tempore ipsum quos odit cumque? Corporis veritatis, laudantium eos fugit nisi iste?</p>
+          <HeaderInFunctieDeCuprins {...cuprins[6]} />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur excepturi iusto itaque harum facere. Sequi nostrum deleniti est. Tempore ipsum quos odit cumque? Corporis veritatis, laudantium eos fugit nisi iste?</p>
+          <HeaderInFunctieDeCuprins {...cuprins[7]} />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur excepturi iusto itaque harum facere. Sequi nostrum deleniti est. Tempore ipsum quos odit cumque? Corporis veritatis, laudantium eos fugit nisi iste?</p>
+          <HeaderInFunctieDeCuprins {...cuprins[8]} />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur excepturi iusto itaque harum facere. Sequi nostrum deleniti est. Tempore ipsum quos odit cumque? Corporis veritatis, laudantium eos fugit nisi iste?</p>
+          <HeaderInFunctieDeCuprins {...cuprins[9]} />
 
-   
-    
-      </Container>
-      {/* <WebsiteSvg /> */}
+          <FormularText text={beneficii} />
+
+        </article></Col>
+        {/* <Col></Col> */}
+      </Row>
 
 
 
 
+    </Container>
+    {/* <WebsiteSvg /> */}
 
 
-      <style jsx>{`
+
+
+
+
+    <style jsx>{`
         
         
         `}</style>
-    </>)
+  </>)
 }
 
 export default Page
