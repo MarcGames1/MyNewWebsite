38772c9912c71998d3dd71f424b918f52d46a0c5
MarcGames1 <George.marcu20@gmail.com>
2021-07-24 05:04:27 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,64 +1,64 @@
-const path = require('path');
-const sitemap = require('nextjs-sitemap-generator');
-const withPlugins = require('next-compose-plugins');
-const withTM = require('next-transpile-modules')([
-  '../components',
-  'react-native-vector-icons',
-  'react-native-modal',
-  'react-native-animatable',
-  'react-native',
-  'react-native-flash-message',
-]);
+// const path = require('path');
+// const sitemap = require('nextjs-sitemap-generator');
+// const withPlugins = require('next-compose-plugins');
+// const withTM = require('next-transpile-modules')([
+//   '../components',
+//   'react-native-vector-icons',
+//   'react-native-modal',
+//   'react-native-animatable',
+//   'react-native',
+//   'react-native-flash-message',
+// ]);
 
-module.exports = withPlugins([
-  [
-    withTM,
-    {
-      distDir: 'build',
-      trailingSlash: true,
-      sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-      },
-      reactStrictMode: true,
-      webpack: config => {
-        config.resolve.alias = {
-          ...(config.resolve.alias || {}),
-          // Transform all direct `react-native` imports to `react-native-web`
-          'react-native$': 'react-native-web',
-        };
-        config.resolve.extensions = [
-          '.web.js',
-          '.web.ts',
-          '.web.tsx',
-          ...config.resolve.extensions,
-        ];
-        config.resolve.modules = [
-          ...config.resolve.modules,
-          path.resolve(__dirname, '../../node_modules'),
-        ];
-        config.module.rules = [
-          {
-            test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
-            loader: 'file-loader',
-            include: path.resolve(
-              __dirname,
-              '../../node_modules/react-native-vector-icons',
-            ),
-          },
-          ...config.module.rules,
-        ];
+// module.exports = withPlugins([
+//   [
+//     withTM,
+//     {
+//       distDir: 'build',
+//       trailingSlash: true,
+//       sassOptions: {
+//         includePaths: [path.join(__dirname, 'styles')],
+//       },
+//       reactStrictMode: true,
+//       webpack: config => {
+//         config.resolve.alias = {
+//           ...(config.resolve.alias || {}),
+//           // Transform all direct `react-native` imports to `react-native-web`
+//           'react-native$': 'react-native-web',
+//         };
+//         config.resolve.extensions = [
+//           '.web.js',
+//           '.web.ts',
+//           '.web.tsx',
+//           ...config.resolve.extensions,
+//         ];
+//         config.resolve.modules = [
+//           ...config.resolve.modules,
+//           path.resolve(__dirname, '../../node_modules'),
+//         ];
+//         config.module.rules = [
+//           {
+//             test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
+//             loader: 'file-loader',
+//             include: path.resolve(
+//               __dirname,
+//               '../../node_modules/react-native-vector-icons',
+//             ),
+//           },
+//           ...config.module.rules,
+//         ];
 
-        return config;
-      },
-    },
-  ],
-]);
+//         return config;
+//       },
+//     },
+//   ],
+// ]);
 
-sitemap({
-  baseUrl: 'https://getvehicool.com',
-  pagesDirectory: __dirname + '/pages',
-  targetDirectory: 'public/static/',
-});
+// sitemap({
+//   baseUrl: 'https://getvehicool.com',
+//   pagesDirectory: __dirname + '/pages',
+//   targetDirectory: 'public/static/',
+// });
 
 // var 2
 
@@ -82,27 +82,27 @@ sitemap({
 
 // var 1
 
-// const withImages = require('next-images')
-// module.exports = withImages()
+const withImages = require('next-images')
+module.exports = withImages()
 
 
-// module.exports = {
-//   reactStrictMode: true,
+module.exports = {
+  reactStrictMode: true,
   
-// }
+}
 
-// const withSvgr = require("next-svgr");
+const withSvgr = require("next-svgr");
  
-// module.exports = withSvgr({
-//   // your config for other plugins or the general next.js here...
-// });
-// module.exports = {
-//   webpack(config) {
-//     config.module.rules.push({
-//       test: /\.svg$/,
-//       use: ["@svgr/webpack"]
-//     });
+module.exports = withSvgr({
+  // your config for other plugins or the general next.js here...
+});
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
 
-//     return config;
-//   }
-// };
+    return config;
+  }
+};
