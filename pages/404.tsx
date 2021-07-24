5c8cef6a54e73c871d08e72f83617b837a4351a5
MarcGames1 <George.marcu20@gmail.com>
2021-07-24 03:15:08 -0700

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,8 +5,10 @@ export default function Custom404() {
     return (<>
     <Alert variant={'danger'}>
 
-    <h1  className='text-center d-block'>Aceasta pagina nu exista</h1>
+    <h1  className='text-center d-block'>A aparut o eroare! </h1>
     </Alert>
+
+    
         <Button href='/'>Acasa</Button>
     
     <style>{`
@@ -15,7 +17,7 @@ export default function Custom404() {
             background-image: url('/img/404/404.jpg');
             background-repeat: no-repeat;
             background-size: contain;
-            background-position: center;
+            background-position: 50% 10%;
         }
 
 
