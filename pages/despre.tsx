e98d1543de02bfc9123021155d83d4fbfd098c01
MarcGames1 <George.marcu20@gmail.com>
2021-08-02 07:57:10 -0700

    x

--- pages/despre.tsx
+++ pages/despre.tsx
@@ -4,14 +4,14 @@ import Image from 'next/image'
 import Text from "../components/Despre Mine/Text"
 import styles from '../styles/Despre.module.css'
 const Despre = () =>{
-const me_img :string = '/img/despre/Eu.jpg'
+const me_img :string = '/img/despre/Eu.webp'
 const bg_img :string =''
     return(<Container fluid>
         <Row className='d-flex align-content-center justify-content-center'>
     <Col md={6}>
     <div className= 'd-flex align-items-center justify-items-center'>
         <div className=' mx-auto my-auto img-thumbnail'>
-        <Image className={styles.img} src={me_img} width={500} height={500}  objectFit='cover'  layout='intrinsic'  />
+        <Image className={styles.img} src={me_img} alt='Alexandru Marcu' width={500} height={500}  objectFit='cover'  layout='intrinsic'  />
         </div>
 
     </div>
