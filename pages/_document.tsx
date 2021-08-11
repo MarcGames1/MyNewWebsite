83ca9c3204b34a6b9b5c9eba4c8e6c296ea61707
MarcGames1 <George.marcu20@gmail.com>
2021-08-12 01:49:42 +0300

    google analitycs + google tag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,11 +12,35 @@ class MyDocument extends Document {
         <Html lang="ro">
 
         <Head>
-       
+  {/* Google Tag Manager */}
+  <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MDZP2HG');`}}></script>
+
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          
+
+          <script  dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', { page_path: window.location.pathname });
+            `,
+          }}
+        />
+        
         </Head>
         <body>
        
-        <div id="fb-root"></div>
+        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MDZP2HG"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
           <Main />
           <NextScript />
           <script type='text/javascript' async defer crossOrigin="anonymous" src="https://connect.facebook.net/ro_RO/sdk.js#xfbml=1&version=v11.0" nonce="CePegJKH"/> 
